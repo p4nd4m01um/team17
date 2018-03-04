6539082b7880b2bc6f829454aec288e0e5e7d9a0
awrfgdg <asdmcn@gmail.com>
2018-03-04 14:13:18 +0000

    test file

---
+++ Code/testt.c
@@ -0,0 +1,83 @@
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#define MAXTIMINGS	85
+#define DHTPIN		7
+int dht11_dat[5] = { 0, 0, 0, 0, 0 };
+ 
+void read_dht11_dat()
+{
+	uint8_t laststate	= HIGH;
+	uint8_t counter		= 0;
+	uint8_t j		= 0, i;
+	float	f; 
+ 
+	dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+ 
+	pinMode( DHTPIN, OUTPUT );
+	digitalWrite( DHTPIN, LOW );
+	delay( 18 );
+	digitalWrite( DHTPIN, HIGH );
+	delayMicroseconds( 40 );
+	pinMode( DHTPIN, INPUT );
+ 
+	for ( i = 0; i < MAXTIMINGS; i++ )
+	{
+		counter = 0;
+		while ( digitalRead( DHTPIN ) == laststate )
+		{
+			counter++;
+			delayMicroseconds( 1 );
+			if ( counter == 255 )
+			{
+				break;
+			}
+		}
+		laststate = digitalRead( DHTPIN );
+ 
+		if ( counter == 255 )
+			break;
+ 
+		if ( (i >= 4) && (i % 2 == 0) )
+		{
+			dht11_dat[j / 8] <<= 1;
+			if ( counter > 16 )
+				dht11_dat[j / 8] |= 1;
+			j++;
+		}
+	}
+ 
+	if ( (j >= 40) &&
+	     (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
+	{	
+		float a, b = dht11_dat[3];
+                b = b / 10;
+                a = dht11_dat[2] * 10 + b;
+                float c,d=dht11_dat[1];
+                d=d/10;
+                c=dht11_dat[0]*10+d;
+		f = dht11_dat[2] * 9. / 5. + 32;
+		printf( "Humidity = %.3f %% Temperature = %.2f C (%.3f F)\n",
+			c, a, f );
+	}
+	else  {
+		printf( "Data not good, skip\n" );
+	}
+}
+ 
+int main( void )
+{
+	printf( "Raspberry Pi wiringPi DHT11 Temperature test program\n" );
+ 
+	if ( wiringPiSetup() == -1 )
+		exit( 1 );
+ 
+	while ( 1 )
+	{
+		read_dht11_dat();
+		delay( 3000 ); 
+	}
+ 
+	return(0);
+}
