356dd099597738bae8b34e35c37178b753e67043
Cao-Jinyu <36344537+Cao-Jinyu@users.noreply.github.com>
2018-03-05 23:56:34 +0000

    Create PIR_sensor.c

---
+++ Code/PIR_sensor.c
@@ -0,0 +1,19 @@
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define PIR		8
+#define motor 9
+
+int Read_PIRsensor()
+{
+pinMode(PIR , INPUT);
+a= digitalRead(PIR);
+return a;
+}
+
+int main ()
+{
+
+}
