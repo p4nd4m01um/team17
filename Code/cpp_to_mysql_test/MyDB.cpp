ebd5870ceff9095d2edf32250904607ec2b4c845
kenzo0619 <kkenzo0619@gmail.com>
2018-03-10 18:27:23 +0000

    test files and calculator

--- Code/cpp_to_mysql_test/MyDB.cpp
+++ Code/cpp_to_mysql_test/MyDB.cpp
@@ -1,9 +1,3 @@
-/*************************************************************************
-> File Name: MyDB.cpp
-> Author:fengxin 
-> Mail:903087053@qq.com 
-> Created Time: 2017年07月21日 星期五 16时02分51秒
-************************************************************************/
 
 #include<iostream>
 #include<string>
@@ -13,7 +7,7 @@ using namespace std;
 
 MyDB::MyDB()
 {
-    mysql=mysql_init(NULL);   //初始化数据库连接变量
+    mysql=mysql_init(NULL);   
     if(mysql==NULL)
     {
         cout<<"Error1:"<<mysql_error(mysql);
@@ -23,7 +17,7 @@ MyDB::MyDB()
 
 MyDB::~MyDB()
 {
-    if(mysql!=NULL)  //关闭数据连接
+    if(mysql!=NULL)  
     {
         mysql_close(mysql);
     }
@@ -32,8 +26,7 @@ MyDB::~MyDB()
 
 bool MyDB::initDB(string host,string user,string passwd,string db_name)
 {
-    // 函数mysql_real_connect建立一个数据库连接  
-    // 成功返回MYSQL*连接句柄，失败返回NULL  
+    
     mysql = mysql_real_connect(mysql, host.c_str(), user.c_str(), passwd.c_str(), db_name.c_str(), 0, NULL, 0);  
     if(mysql == NULL)  
     {  
@@ -45,26 +38,26 @@ bool MyDB::initDB(string host,string user,string passwd,string db_name)
 
 bool MyDB::exeSQL(string sql)
 {
-    //mysql_query()执行成功返回0,执行失败返回非0值。
+    
     if (mysql_query(mysql,sql.c_str()))
     {
         cout<<"Query Error3: "<<mysql_error(mysql);
         return false;
     }
-    else // 查询成功
+    else 
     {
-        result = mysql_store_result(mysql);  //获取结果集
-        if (result)  // 返回了结果集
+        result = mysql_store_result(mysql);  
+        if (result)  
         {
-           int  num_fields = mysql_num_fields(result);   //获取结果集中总共的字段数，即列数
-           int  num_rows=mysql_num_rows(result);       //获取结果集中总共的行数
-           for(int i=0;i<num_rows;i++) //输出每一行
+           int  num_fields = mysql_num_fields(result);   
+           int  num_rows=mysql_num_rows(result);       
+           for(int i=0;i<num_rows;i++) 
             {
-                //获取下一行数据
+                
                 row=mysql_fetch_row(result);
                 if(row<0) break;
 
-                for(int j=0;j<num_fields;j++)  //输出每一字段
+                for(int j=0;j<num_fields;j++) 
                 {
                     cout<<row[j]<<"\t\t";
                 }
@@ -74,10 +67,10 @@ bool MyDB::exeSQL(string sql)
         }
         else  // result==NULL
         {
-            if(mysql_field_count(mysql) == 0)   //代表执行的是update,insert,delete类的非查询语句
+            if(mysql_field_count(mysql) == 0)   
             {
                 // (it was not a SELECT)
-                int num_rows = mysql_affected_rows(mysql);  //返回update,insert,delete影响的行数
+                int num_rows = mysql_affected_rows(mysql);  
             }
             else // error
             {
--- Code/cpp_to_mysql_test/MyDB.h
+++ Code/cpp_to_mysql_test/MyDB.h
@@ -1,9 +1,3 @@
-/*************************************************************************
-    > File Name: mydb.h
-    > Author:fengxin 
-    > Mail:903087053@qq.com 
-    > Created Time: 2017年07月21日 星期五 15时17分17秒
- ************************************************************************/
 
 #ifndef _MYDB_H
 #define _MYDB_H
@@ -17,12 +11,12 @@ class MyDB
     public:
     MyDB();
     ~MyDB();
-    bool initDB(string host,string user,string pwd,string db_name); //连接mysql
-    bool exeSQL(string sql);   //执行sql语句
+    bool initDB(string host,string user,string pwd,string db_name); 
+    bool exeSQL(string sql);   
     private:
-    MYSQL *mysql;          //连接mysql句柄指针
-    MYSQL_RES *result;    //指向查询结果的指针
-    MYSQL_ROW row;       //按行返回的查询信息
+    MYSQL *mysql;          
+    MYSQL_RES *result;    
+    MYSQL_ROW row;       
 };
 
 
