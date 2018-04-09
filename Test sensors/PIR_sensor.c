96eeab03909c1da2b9c8321491a761ef16c125c7
Cao-Jinyu <36344537+Cao-Jinyu@users.noreply.github.com>
2018-04-09 22:57:26 +0100

    Update PIR_sensor.c

--- Test sensors/PIR_sensor.c
+++ Test sensors/PIR_sensor.c
@@ -9,13 +9,13 @@
 int Read_PIRsensor()
 {
   sleep(2);                    //to stablize the sensor
-int a;
+int a=0;
 pinMode(PIR , INPUT);
 a= digitalRead(PIR);
   if (a==1)
-    printf("motion detected"\n);
+    printf("motion detected \n");
   else
-    printf("nothing detected"\n);
+    printf("nothing detected \n");
   // sleep (2);
 return a;
 }
